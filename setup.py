895477854f5ead8175a62b34cf39969ed6e6cf7b
Roger Almeida <roger.eduardo@gmail.com>
2018-01-09 20:07:35 +1100

    Added more informations to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,16 @@ setup(
     entry_points='''
         [console_scripts]
         pomodoro=pomodoro:cli
-    '''
-        )
+    ''',
+    author='Roger Almeida',
+    author_email="roger.eduardo@gmail.com",
+    description="Pomodoro timer",
+    license="MIT",
+    keywords="pomodoro technique timer application cli command line tool",
+    url="https://github.com/rogeralmeida/pomodoro",
+    project_urls={
+        "Bug Tracker": "https://github.com/rogeralmeida/pomodoro/issues",
+        "Documentation": "https://github.com/rogeralmeida/pomodoro",
+        "Source Code": "https://github.com/rogeralmeida/pomodoro",
+    }
+)
